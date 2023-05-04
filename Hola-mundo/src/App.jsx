e65b27ffbeb3d7c20a17e8de3a72d9feac21a245
Diego Pallares Git <fercho.182@hotmail.com>
2023-05-04 15:52:32 -0500

    Implementacion estado: useState

--- Hola-mundo/src/App.jsx
+++ Hola-mundo/src/App.jsx
@@ -3,15 +3,35 @@ import './App.css'
 import './index.css'
 import { ComponetTwitterFollowCard } from  './TwitterFollowCard.jsx'
 export function App(){
+    
     return(
-        <React.Fragment>
-            <ComponetTwitterFollowCard logo="1" userName="Dog-Alfha" name="Perro motero"/>
-            <ComponetTwitterFollowCard logo="1" userName="Dog-2" name="Perro motero"/>
-            <ComponetTwitterFollowCard logo="1" userName="Dog-5" name="Perro moterazo"/>
-            <ComponetTwitterFollowCard logo="1" userName="Dog-Alfha" name="Perro motero"/>
-            <ComponetTwitterFollowCard logo="1" userName="Dog-Alfha" name="Perro motero"/>
-            <ComponetTwitterFollowCard logo="1" userName="Dog-Alfha" name="Perro motero"/>
-        </React.Fragment>
+        <section className='App'>
+            <React.Fragment>
+                <ComponetTwitterFollowCard
+                    isFollowing={true}
+                    logo="1"
+                    userName="Dog-Alfha"
+                    name="Perro motero"/>
+                <ComponetTwitterFollowCard
+                    isFollowing={false}
+                    logo="2"
+                    userName="Dog-2"
+                    name="Perro motero"/>
+                <ComponetTwitterFollowCard
+                    isFollowing={true}
+                    logo="3"
+                    userName="Dog-5"
+                    name="Perro moterazo"
+                />
+                <ComponetTwitterFollowCard
+                    isFollowing={true}
+                    logo="4"
+                    userName="Dog-Alfha"
+                    name="Perro motero"
+                 />
+            </React.Fragment>
+        </section>
+        
     )
 }
       
--- Hola-mundo/src/TwitterFollowCard.jsx
+++ Hola-mundo/src/TwitterFollowCard.jsx
@@ -1,23 +1,37 @@
-import logo from './images/perro-viajero-1.jpg'
-
+import { useState } from 'react'
 import './App.css'
 import './index.css'
 
-export function ComponetTwitterFollowCard ({userName, name }){
+export function ComponetTwitterFollowCard ({userName, logo, name}){
+    
+    //*Debe quedar el estado creado en la parte superior.
+    //Primera posicion "valor del estado" y segunda posicion "Forma de actualizarlo"
+    const [isFollowing, setIsFollowing] = useState(false)
+   
+    //*Las constasten que tomaran un valor dependiendo del ternario
+    const text = isFollowing? 'siguiendo' : 'seguir'
+    const buttonClassName = isFollowing ? 'tw-followCard-button is-following':'tw-followCard-button'
+    
+    //*La ejecucion del cambio del estado.
+    const handClick = () =>{
+        setIsFollowing(!isFollowing)
+    }
+    
+    
     return(
         <article className='tw-followCard'>
-        <header className='ttw-followCard-avatar'>
+        <header className='tw-followCard-hader'>
             <img 
             className='tw-followCard-avatar'
             alt="Avatar de perro 1"
-            src={logo}/>
+            src={`src/images/perro-viajero-${logo}.jpg`}
+            />
             <div className='tw-followCard-info'>
                 <strong>{name}</strong>
                 <span className='tw-followCard-infoUserName'>@{userName}</span>
             </div>
-
             <aside>
-                <button className='tw-followCard-button'>seguir</button>
+                <button className={buttonClassName} onClick={handClick}>{text}</button>
             </aside>
         </header>
     </article>
